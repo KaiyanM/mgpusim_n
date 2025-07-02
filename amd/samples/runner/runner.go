26829e76f6fef6d725be21023e8ccc9583084998
KaiyanM <kaiiyenma@gmail.com>
2025-07-02 15:58:09 +0800

    Update runner.go

--- amd/samples/runner/runner.go
+++ amd/samples/runner/runner.go
@@ -97,13 +97,13 @@ func (r *Runner) buildTimingPlatform() {
 }
 
 func (r *Runner) configureVisTracing() {
-	if !*visTracing {
-		return
-	}
-
 	visTracer := r.simulation.GetVisTracer()
 	for _, comp := range r.simulation.Components() {
 		tracing.CollectTrace(comp.(tracing.NamedHookable), visTracer)
+	}
+
+	if *visTracing {
+		visTracer.EnableTracing() // 如果有*visTracing，打开
 	}
 }
 
